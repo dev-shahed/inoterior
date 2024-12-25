5eecc41abc81fb9b7f272dcfef0c22ac1baf2a09
dev-shahed <shahedthedev@gmail.com>
2024-12-25 19:07:15 +0600

    update

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'export',
   images: {
     domains: ['images.pexels.com', 'images.unsplash.com', 'arcon.s3.amazonaws.com'],
   },
-};
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    unoptimized: true, // Disable Next.js image optimization for static exports
-  },
-  output: 'export', // Ensure this is for static export
+  output: 'standalone', // For server-side rendering
 };
 
 module.exports = nextConfig;
